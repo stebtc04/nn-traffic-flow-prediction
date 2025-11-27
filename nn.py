a4cdea4ce4c0b4760a68d3ee3f8e3279836a281f
stefanodesaraca <stefanodesaraca@gmail.com>
2025-11-27 10:02:29 +0100

    Project foundations 3

--- nn.py
+++ nn.py
@@ -1,13 +1,22 @@
+from pathlib import Path
 import random
 import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
 
-from pytorch_forecasting import TemporalFusionTransformer
+import torch
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import EarlyStopping, LearningRateMonitor
+from lightning.pytorch.loggers import TensorBoardLogger
+from pytorch_forecasting import Baseline, TemporalFusionTransformer, TimeSeriesDataSet
+from pytorch_forecasting.models.temporal_fusion_transformer.tuning import (
+    optimize_hyperparameters,
+)
 
-
+warnings.filterwarnings("ignore")  # avoid printing out absolute paths
 
 
 
